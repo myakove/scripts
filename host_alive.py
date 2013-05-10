cdee095da84df311a05eea9fe7f542f8d2cb71b1
Meni Yakove <myakove@redhat.com>
2013-05-10 16:16:54 +0300

    FIX

--- host_alive.py
+++ host_alive.py
@@ -1,13 +1,13 @@
-import paramiko
+#! /usr/bin/python
+
 import os
 
-hosts_list_org = open("/home/myakove/pdsh-files/all-network-hosts", "r")
-hosts_list = [line.strip() for line in hosts_list_org]
-ssh = paramiko.SSHClient()
-ssh.load_system_host_keys()
 
-for host in hosts_list:
-    if not os.system("ssh -o ConnectTimeout=5 root@" + host + " exit"):
-        print "%s is alive" % host
-    else:
-        print "%s is down" % host
+def hostAlive():
+    hosts_list_org = open("/home/myakove/pdsh-files/all-network-hosts", "r")
+    hosts_list = [line.strip() for line in hosts_list_org]
+    for host in hosts_list:
+        if not os.system("ssh -o ConnectTimeout=5 root@" + host + " exit"):
+            print "%s is alive" % host
+        else:
+            print "%s is down" % host
--- update-hosts-repo.py
+++ update-hosts-repo.py
@@ -4,27 +4,29 @@ import os
 import user
 
 input = raw_input("\033[0;32m" + 'Build: ' + "\033[0m")
-hosts_file = "network-hosts"
+
 home = user.home
+hosts_file = "network-hosts"
 hosts_path = home + "/pdsh-files/" + hosts_file
+user = "root"
+repo_dir = "/etc/yum.repos.d/"
+tmp_file = "/tmp/rhevm.repo"
 host_list = open(hosts_path, "r")
 host_target_list = [line.strip() for line in host_list]
 host_target = ",".join(host_target_list)
-repo_dir = "/etc/yum.repos.d/"
-tmp_file = "/tmp/rhevm.repo"
-domain = ".qa.lab.tlv.redhat.com"
-user = "root"
 
 
 def updateRepo():
     repo_file = open(tmp_file, "w")
     repo_file.write("[rhevm]" + "\n")
     repo_file.write("name=RHEVM" + "\n")
-    repo_file.write("baseurl=http://bob.eng.lab.tlv.redhat.com/builds/" + input + "\n")
+    repo_file.write("baseurl=http://bob.eng.lab.tlv.redhat.com/builds/" +
+                    input + "\n")
     repo_file.write("gpgcheck=0" + "\n")
     repo_file.write("enable=1" + "\n")
     repo_file.close()
-    os.system("pdcp -w " + host_target + " -l " + user + " " + tmp_file + " " + repo_dir)
+    os.system("pdcp -w " + host_target + " -l " + user + " " + tmp_file + " " +
+              repo_dir)
 
 
 def updateHosts():
