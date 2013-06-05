ec56dfa2fc3a234f5a1f634903fd66675bbdb10a
Meni yakove <myakove@gmail.com>
2013-06-05 23:33:38 +0300

    git-pull fix

--- python/git-pull.py
+++ python/git-pull.py
@@ -4,19 +4,54 @@ from subprocess import PIPE
 import subprocess
 import os
 import argparse
+import re
 
 user_input = argparse.ArgumentParser()
 user_input.add_argument('--git', '-G', help="path to git folder")
+user_input.add_argument("--repo", "-R", help="Repo folder to update, use " +
+                        "'folder1 folder2' for multipale folders")
+
 option = user_input.parse_args()
 
-if not option.git:
-    print "Git path must be specify"
+'''
+def findInList(list1, list2):
+    for val in git_repos:
+        status = 0
+        if val not in git_folders:
+            print "%s not found in %s folder" % (val, option.git)
+            status = 1
+    if status is 1:
+        return False
+    return True
+'''
+def findInList(list1, list2):
+    repo_list = []
+    status = 1
+    for val in git_repos:
+        if val in git_folders:
+            repo_list.append(val)
+            status = 0
+        else:
+            print "%s not found in %s folder" % (val, option.git)
+    return status, repo_list
+
+
+
+
+
+if not (option.git and option.repo):
+    print "Git path and repo folder must be specify"
     print user_input.format_usage()
 
 else:
+    git_repos = option.repo.split()
     git_folders = subprocess.Popen(["ls", option.git], stdout=PIPE,
                                    stderr=PIPE).communicate()[0].split()
-    for val in git_folders:
-        os.chdir(option.git + val)
-        print "\033[0;32m" + os.getcwd().split('/')[4] + "\033[0m"
-        os.system("git pull")
+    rc, repo_list = findInList(git_folders, git_repos)
+    if rc == 0:
+        for val in repo_list:
+            os.chdir(option.git + "/" + val)
+            print "\033[0;32m" + os.getcwd().split('/')[4] + "\033[0m"
+            os.system("git checkout master")
+            os.system("git pull")
+
